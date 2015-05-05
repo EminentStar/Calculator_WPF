71dcd445d1df97cbdae15e96b8730f65bd23b871
EminentStar <pjk3843@gmail.com>
2015-05-06 02:50:31 +0900

    06/05/2015

--- Calculator/MainControl.xaml.cs
+++ Calculator/MainControl.xaml.cs
@@ -21,14 +21,15 @@ namespace Calculator
     public partial class MainControl : UserControl
     {
         private MSScriptControl.ScriptControl sc = new MSScriptControl.ScriptControl();
-        //sc.Language = "VBscript";
         private string expression;
         private object result;
         private string inputStr;
         private string lastOperator;
         private string lastOperand;
+        private int numLen;
         private bool isFinished;
         private bool ZeroOperator;
+        private string lastChar;
 
         public MainControl()
         {
@@ -36,6 +37,7 @@ namespace Calculator
             sc.Language = "VBscript";
             currentTxt.Text = "0";
             ZeroOperator = true;
+            numLen = 1;
 
             btn_1.Click += num_Click; btn_2.Click += num_Click; btn_3.Click += num_Click; btn_4.Click += num_Click; btn_5.Click += num_Click;
             btn_6.Click += num_Click; btn_7.Click += num_Click; btn_8.Click += num_Click; btn_9.Click += num_Click; btn_0.Click += num_Click;
@@ -51,38 +53,25 @@ namespace Calculator
             if (isFinished) // The variable isFinished's value is true; mind you, you gotta clear the original value and then you need to newly start inserting values
                 currentTxt.Text = "0";
             isFinished = false;
-            string lastChar = currentTxt.Text.Substring(currentTxt.Text.Length - 1, 1);
+            lastChar = currentTxt.Text.Substring(currentTxt.Text.Length - 1, 1);
             
 
-            if (currentTxt.Text.Equals("0") || (lastChar.Equals("/") || lastChar.Equals("*") || lastChar.Equals("+") || lastChar.Equals("-")))
+            if (currentTxt.Text.Equals("0") || (lastChar.Equals("/") || lastChar.Equals("*") || lastChar.Equals("+") || lastChar.Equals("-"))) //EH002
+            {
                 currentTxt.Text = paramNum;
+                numLen = 1;
+            }
             else
+            {
                 currentTxt.Text += paramNum;
+                numLen++;
+            }
+                
         }
 
         public void OperatorCheck(object sender, RoutedEventArgs e)
         {
-            //lastOperand = currentTxt.Text;
-            //inputStr = GetExressionElement(e);
-            //string lastChar = "";
-
-            //if(!resultTxt.Text.Equals(""))
-            //    lastChar = resultTxt.Text.Substring(resultTxt.Text.Length - 1, 1);
-
-            //if (lastChar.Equals("/") || lastChar.Equals("*") || lastChar.Equals("+") || lastChar.Equals("-"))
-            //{
-            //    resultTxt.Text = resultTxt.Text.Remove(resultTxt.Text.Length - 1);
-            //    resultTxt.Text += inputStr;
-            //}
-            //else
-            //{
-            //    result = sc.Eval(resultTxt.Text + currentTxt.Text);
-            //    resultTxt.Text += currentTxt.Text + inputStr;
-            //    currentTxt.Text = result.ToString();
-            //}
-            //isFinished = true;
-            //lastOperator = inputStr;
-
+            numLen = 1; //related to EH001
             lastOperand = currentTxt.Text;
             inputStr = GetExressionElement(e);
             string lastChar = "";
@@ -94,6 +83,7 @@ namespace Calculator
             }
             else
             {
+                numLen = 1;
                 if(!ZeroOperator)
                 {
                     result = sc.Eval(resultTxt.Text + currentTxt.Text);
@@ -102,7 +92,7 @@ namespace Calculator
                 }
                 else
                 {
-                    resultTxt.Text += currentTxt + inputStr;
+                    resultTxt.Text += currentTxt.Text + inputStr;
                     ZeroOperator = false;
                 }
             }
@@ -114,12 +104,20 @@ namespace Calculator
         public void dot_check()
         {
             if (!currentTxt.Text.Contains("."))
+            {
                 currentTxt.Text += ".";
+                numLen = 0; //EH003
+            }
+                
         }
         public void num_Click(object sender, RoutedEventArgs e)
         {
-            inputStr = GetExressionElement(e);
-            InitializedCheckAndInsert(inputStr);
+            if(numLen<16) //Exception Handling EH001
+            {
+                inputStr = GetExressionElement(e);
+                InitializedCheckAndInsert(inputStr);
+            }
+            
         }
 
         //Get the string of what I've clicked just now
@@ -140,12 +138,14 @@ namespace Calculator
             result = sc.Eval(expression);
             currentTxt.Text = result.ToString();
             resultTxt.Text = null;
+            isFinished = true;
         }
         public void clear_Click(object sender, RoutedEventArgs e)
         {
             resultTxt.Text = null; 
-            currentTxt.Text = "0";
+            currentTxt.Text = "0"; 
             ZeroOperator = true;
+            numLen = 1;
         }
         public void dot_Click(object sender, RoutedEventArgs e)
         {
